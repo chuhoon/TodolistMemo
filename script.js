b5eb0530544e0c922861a673bf86c8d1c6bad8e5
chuhoon <khchu1220@naver.com>
2022-05-27 21:58:53 +0900

    Make localStorage

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const todoList = document.querySelector('.cont-list');
 const inputForm = document.querySelector('.input-form');
 todoInput = inputForm.querySelector('.input-text');
 
-let todoListArr = [];
-let listI = 0;
+const todoListArr = [];
+const listI = 0;
 
 function getTime() {
   const time = new Date();
@@ -37,8 +37,8 @@ function setTodoList(text) {
 }
 
 function getTodoList(text) {
-  const getStr = localStorage.getItem('key');
-  const parseGetStr = JSON.parse(getStr);
+  // const getStr = localStorage.getItem('key');
+  // const parseGetStr = JSON.parse(getStr);
 
   const elli = document.createElement('li');
   const checkBtn = document.createElement('div');
@@ -60,8 +60,6 @@ function getTodoList(text) {
   todoList.appendChild(elli);
   completeTodo(checkBtn, elp);
   clearBtn.addEventListener('click', deletelist);
-
-  console.log(parseGetStr);
 }
 
 function handleSubmit(event) {
@@ -71,42 +69,6 @@ function handleSubmit(event) {
   getTodoList(myList);
   todoInput.value = '';
 }
-
-// function pushText() {
-//   window.addEventListener('keydown', (e) => {
-//     if (e.key == 'Enter' && document.getElementById('input-text').value != '') {
-//       todoListArr.push(document.getElementById('input-text').value);
-//       document.getElementById('input-text').value = '';
-//       setTodoList();
-//       createTodoList();
-//       listI += 1;
-//     }
-//   });
-// }
-
-// function createTodoList() {
-//   const elli = document.createElement('li');
-//   const checkBtn = document.createElement('div');
-//   const elp = document.createElement('p');
-//   const clearBtn = document.createElement('div');
-//   clearBtn.innerText = 'x';
-
-//   elli.classList.add('list-item');
-//   checkBtn.classList.add('btn-checkBox');
-//   elp.classList.add('p-data');
-//   clearBtn.classList.add('btn-clear');
-//   getTodoList();
-
-//   elp.innerText = todoListArr[listI];
-
-//   elli.appendChild(checkBtn);
-//   elli.appendChild(elp);
-//   elli.appendChild(clearBtn);
-
-//   todoList.appendChild(elli);
-//   completeTodo(checkBtn, elp);
-//   clearBtn.addEventListener('click', deletelist);
-// }
 
 function completeTodo(checkBtn, elp) {
   checkBtn.addEventListener('click', function () {
